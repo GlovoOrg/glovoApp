724ba6e44ae52155bb3d34d193b8b9cd7a24ebaf
Alinur25 <bestmenchik1338@gmail.com>
2025-02-13 12:13:39 +0600

    Added filtration and changed payment_details

---
+++ src/main/java/com/api/glovoCRM/Specifications/EstablimentSpecifications/SubcategorySpecification.java
@@ -0,0 +1,10 @@
+package com.api.glovoCRM.Specifications.EstablimentSpecifications;
+
+import com.api.glovoCRM.Models.EstablishmentModels.SubCategory;
+import com.api.glovoCRM.Specifications.BaseSpecification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubcategorySpecification extends BaseSpecification<SubCategory> {
+
+}
